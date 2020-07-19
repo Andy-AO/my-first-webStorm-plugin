bf5795f4e57fc81ed60011bdf90467546b28ab6e
GitHub Action <action@github.com>
2020-07-19 01:09:28 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/oilj/myfirstwebstormplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.oilj.myfirstwebstormplugin.services
+
+import com.github.oilj.myfirstwebstormplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/oilj/myfirstwebstormplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/oilj/myfirstwebstormplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.oilj.myfirstwebstormplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.oilj.myfirstwebstormplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
